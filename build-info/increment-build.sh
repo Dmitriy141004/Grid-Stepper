9d76b48389eede8df0a2a12000a38cc57c8ad142
dmitriy <!Lbvf141004!>
2016-03-25 13:56:59 +0200

    Small changes

--- build-info/increment-build.sh
+++ build-info/increment-build.sh
@@ -4,17 +4,17 @@
 
 cd `pwd`/`dirname $0`
 
-if [ ! -f .build_num ] ; then          # If file not exists, we must create it
+if [ ! -f .build_num ] ; then            # If file not exists, we must create it
     touch .build_num
     cd ../                               # Adding file to git
     git -c core.quotepath=false add -- build-info/.build_num
     cd build-info
 fi
 
-BUILD_NUM=`cat .build_num`             # Getting build number
+BUILD_NUM=`cat .build_num`               # Getting build number
 if [ "${BUILD_NUM}" == "" ] ; then       # If number is empty "string", it is set to zero
     BUILD_NUM=0
 fi
 BUILD_NUM=$((BUILD_NUM+1))               # Incrementing build number
 
-echo ${BUILD_NUM} > .build_num         # Writing to file
+echo ${BUILD_NUM} > .build_num           # Writing to file
